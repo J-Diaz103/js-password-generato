2407e9d241f5074e93e96dcd84a677536d505d03
J Diaz <j.e.diaz@icloud.com>
2022-01-01 20:20:51 -0800

    everything seems to be working correctly

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// to randomize the selection try to concat the arrays and use for loop to select from the options
-// const arr1 = ["Cecilie", "Lone"];
-// const arr2 = ["Emil", "Tobias", "Linus"];
-// const arr3 = ["Robin"];
-// const children = arr1.concat(arr2,arr3);
-
-//  random choices
-// var random = choices[Math.floor(Math.random() * choices.length)];
-
 //List of the special chars that can be used for password
-// var specialChars = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
-// var nubers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-// var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialChars = ['special'];
-var nubers = ["numbers"];
-var lowerCase = ["lower case"];
-var upperCase = ["upper case"];
-
+var specialChars = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+var nubers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var mixed = [];
 
@@ -28,47 +14,68 @@ function generatePassword() {
   var start = prompt('How many character do you want?')
 
   if (isNaN(start)) {
-    console.log("Invalid")
+    alert("input must be a number");
     return;
   };
-  var addSpecial = confirm("include special characters?")
-  var addNumbers = confirm("include Numbers?")
-  var addlowecase = confirm("include lowercase characters?")
-  var adduppercase = confirm("include uppercase characters?")
+  if (start < 8 || start > 128) {
+    alert("number must be at least 8 characters but less than 128");
+    return;
+  };
 
-  //all choices
-  // if skip a choice it  still shows up in the mixed arrary
-  //need to debug
-  if (addSpecial, addNumbers, addlowecase, adduppercase) {
-    var choices = mixed.concat(specialChars, nubers, lowerCase, upperCase);
-  } else if (addNumbers, addlowecase, adduppercase) {
-    var choices = mixed.concat(nubers, lowerCase, upperCase);
-  } else if (addlowecase, adduppercase) {
-    var choices = mixed.concat(lowerCase, upperCase);
-  } else if (adduppercase) {
-    var choices = mixed.concat(upperCase);
-  } else if (addSpecial, addNumbers, addlowecase) {
-    var choices = mixed.concat(specialChars, nubers, lowerCase);
-  } else if (addSpecial, addNumbers) {
-    var choices = mixed.concat(specialChars, nubers);
-  } else if (addSpecial) {
-    var choices = mixed.concat(specialChars);
-  } else if (addNumbers, addlowecase) {
-    var choices = mixed.concat(nubers, lowerCase);
-  } else if (addNumberse) {
-    var choices = mixed.concat(nubers);
-  } else if (addlowecase) {
-    var choices = mixed.concat(lowerCase);
-  } else if (addSpecial, !addNumbers, addlowecase, !adduppercase) {
-    var choices = mixed.concat(specialChars, lowerCase);
-  } else if (addNumbers, adduppercase) {
-    var choices = mixed.concat(nubers, upperCase);
+  var addSpecial = confirm("include special characters?");
+  var addNumbers = confirm("include Numbers?");
+  var addlowecase = confirm("include lowercase characters?");
+  var adduppercase = confirm("include uppercase characters?");
+
+  if (!addSpecial && !addNumbers && !addlowecase && !adduppercase) {
+    alert("must select at least one");
+    return;
   }
 
-  console.log(choices)
+  if (addSpecial === true) {
+    var choices = mixed.push(specialChars);
+  }
+  if (addNumbers === true) {
+    var choices = mixed.push(nubers);
+  }
+  if (addlowecase === true) {
+    var choices = mixed.push(lowerCase);
+  }
+  if (adduppercase === true) {
+    var choices = mixed.push(upperCase);
+  }
 
+  // check progress
+  // console.log("mixed " + mixed);
+  // console.log("choices " + choices);
+  // console.log("password lenght " + start);
 
-  return;
+  var possiblePassword = [];
+
+  if (mixed.includes(specialChars)) {
+    var possiblePassword = possiblePassword.concat(specialChars);
+  }
+  if (mixed.includes(nubers)) {
+    var possiblePassword = possiblePassword.concat(nubers);
+  }
+  if (mixed.includes(lowerCase)) {
+    var possiblePassword = possiblePassword.concat(lowerCase);
+  }
+  if (mixed.includes(upperCase)) {
+    var possiblePassword = possiblePassword.concat(upperCase);
+  }
+  console.log(`possiblePassword : ${possiblePassword}`);
+  var partialPassword = [];
+  var finalPassword = [];
+  for (var i = 0; i < start; i++) {
+    var partialPassword = possiblePassword[Math.floor(Math.random() * possiblePassword.length)];
+    finalPassword = finalPassword.concat(partialPassword);
+  }
+
+  console.log(partialPassword, i);
+  console.log(finalPassword.join(""));
+
+  return finalPassword.join("");
 }
 
 // Write password to the #password input
@@ -82,4 +89,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
